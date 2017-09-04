901b54f6861d63378d0f9a929748c3068c24e88c
Brantly Callaway <brant.callaway@gmail.com>
2017-09-04 10:11:38 -0400

    fixed some bugs

--- R/attcpo.R
+++ R/attcpo.R
@@ -200,6 +200,8 @@ compute.attcpo <- function(Y1t, Y0tmin1, Y0tmin2,
 #'                t=2011, tmin1=2007, tname="year",
 #'                idname="id", panel=TRUE, data=displacements,
 #'                probs=seq(.05,.95,.01),se=FALSE)
+#' cc$F.treated.tmin1 <- ecdf(subset(displacements, year==2007 & treat==1)$learn)
+#' cc$F.treated.tmin2 <- ecdf(subset(displacements, year==2003 & treat==1)$learn)
 #' ac <- attcpo(learn ~ treat, 2011, 2007, 2003, "year", displacements,
 #'         "id", cc, method="rank", yseqlen=10)
 #' ac
